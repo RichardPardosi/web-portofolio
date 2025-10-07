91a4f414b96d415d732b67ada72fefbd2c53ca23
RichardPardosi <richard12eduardo@gmail.com>
2025-10-07 23:30:13 +0700

    fix js script

---
+++ main.js
@@ -0,0 +1,33 @@
+        document.addEventListener("DOMContentLoaded", function () {
+        const toggle = document.getElementById("darkModeToggle");
+        const body = document.body;
+
+        // Cek status dari localStorage
+        if (localStorage.getItem("dark-mode") === "enabled") {
+            body.classList.add("dark-mode");
+            toggle.innerHTML = "☀️ Light Mode";
+            toggle.classList.remove("btn-outline-light");
+            toggle.classList.add("btn-outline-warning");
+        } else {
+            toggle.innerHTML = "🌙 Dark Mode";
+            toggle.classList.remove("btn-outline-warning");
+            toggle.classList.add("btn-outline-light");
+        }
+
+        // Saat tombol diklik
+        toggle.addEventListener("click", () => {
+            const isDark = body.classList.toggle("dark-mode");
+
+            if (isDark) {
+            localStorage.setItem("dark-mode", "enabled");
+            toggle.innerHTML = "☀️ Light Mode";
+            toggle.classList.remove("btn-outline-light");
+            toggle.classList.add("btn-outline-warning");
+            } else {
+            localStorage.setItem("dark-mode", "disabled");
+            toggle.innerHTML = "🌙 Dark Mode";
+            toggle.classList.remove("btn-outline-warning");
+            toggle.classList.add("btn-outline-light");
+            }
+        });
+        });
